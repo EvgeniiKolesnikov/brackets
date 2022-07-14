ce03dd0a69f0c94099c5919295aaed001272073c
Evgenii Kolesnikov <jekow@yandex.ru>
2022-07-14 03:05:49 +0300

    Done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const brackets = bracketsConfig.map((arr) => arr.join(''));
+  
+  let tempStr = str;
+  let newStr = '';
+
+  // console.log('brackets = ', brackets);
+  // console.log('str = ', str);
+  // let iteraion = 0;
+
+  while (newStr !== tempStr) {
+    // console.log('iteraion = ', ++iteraion);
+    newStr = tempStr;
+    brackets.map((bracket) => {
+      // console.log('bracket = ', bracket);
+      while (tempStr.indexOf(bracket) !== -1) {
+        tempStr = tempStr.replace(bracket, '');
+        // console.log('tempStr = ', tempStr);
+      }
+    });
+  }
+
+  // console.log('newStr = ', newStr);
+  return newStr === '' ? true : false;
+};
